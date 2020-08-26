8cdd539d3e3769db21f40ddb9034ce8712c9e80f
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-08-26 22:39:47 +0700

    update config

--- src/Config/Cart.php
+++ src/Config/Cart.php
@@ -6,44 +6,28 @@ use CodeIgniter\Config\BaseConfig;
 
 class Cart extends BaseConfig
 {
-   /*
-   |--------------------------------------------------------------------------
-   | Default tax rate
-   |--------------------------------------------------------------------------
-   |
-   | This default tax rate will be used when you make a class implement the
-   | Taxable interface and use the HasTax trait.
-   |
-   */
+    /**
+     * This default tax rate will be used when you make a class implement the
+     * taxable interface and use the HasTax trait.
+     */
     public $tax = 21;
 
-   /*
-   |--------------------------------------------------------------------------
-   | Shoppingcart table settings
-   |--------------------------------------------------------------------------
-   |
-   | Here you can set the connection that the shoppingcart should use when
-   | storing and restoring a cart.
-   |
-   */
+    /**
+     * Here you can set the connection that the shoppingcart should use when
+     * storing and restoring a cart.
+     */
     public $table = 'shoppingcart';
 
-   /*
-   |--------------------------------------------------------------------------
-   | Default number format
-   |--------------------------------------------------------------------------
-   |
-   | This defaults will be used for the formated numbers if you don't
-   | set them in the method call.
-   |
-   */
+    /**
+     * This defaults will be used for the formated numbers if you don't
+     * set them in the method call.
+     */
     public $format = [
 
-      'decimals' => 2,
+        'decimals' => 2,
 
-      'decimal_point' => '.',
+        'decimal_point' => '.',
 
-      'thousand_seperator' => ','
-
+        'thousand_seperator' => ',',
     ];
 }
